5ef525927b84c8cbf52aaa2bf50abb00a07d17a6
lurch <alexhann17@yahoo.com>
2022-02-12 16:35:18 -0800

    editing routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,87 +1,48 @@
-const router = require("express").Router();
-const { Workout } = require("../models/schema");
-const path = require('path');
-//show the exercise webpage
-router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+const db = require("../models");
+const path = require("path");
 
-  });
-
-//show the stats webpage
-router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
-  });
-
-
-router.get('/api/workouts/range', (req, res) => {
-    Workout.find().sort({ weight: -1 }).limit(7)
-    .then(dbWorkouts => {
-        console.log(dbWorkouts)
-        res.json(dbWorkouts)
-    })
-    .catch(err => {
-        console.log("RANGE ERROR", err)
-        res.status(400).json(err);
-    });
-})
-
-//get all workouts
-router.get("/", (req, res) => {
-    Workout.find({})
-      .then((workoutData) => {
-        res.json(workoutData);
-      })
-      .catch((error) => {
-        res.json(error);
-      });
-  });
-
-//get a specific workout
-
-router.get("/api/workouts/:id", ({ params, body }, res) => {
-    Workout.findById({ _id: params.id })
-      .then((workoutData) => res.json(workoutData))
-      .catch((error) => res.status(400).json(error));
-  });
-
-//get most recent workout
-router.get('/api/workouts', (req, res) => {
-    Workout.find({}, {}, { sort: { 'created_at' : -1 } })
-    .then(dbWorkout => {
-        console.log(dbWorkouts)
-        res.json(dbWorkouts)
-    })
-    .catch(err => {
-        console.log(err)
-      res.status(400).json(err);
-    });
-    })
-
-//add exercises
-router.post('/api/workouts', ({ body }, res) => {
-    Workout.create({})
-    .then(dbWorkout => {
-        console.log(dbWorkouts)
-      res.json(dbWorkouts);
-    })
-    .catch(err => {
-        console.log(err)
-      res.status(400).json(err);
-    });
-})
-
-//update workout
-router.put('/api/workouts/:id', ({body, params}, res) => {
-    Workout.findByIdAndUpdate(params.id, 
-        {$push: {exercises: body}},
-        {new: true, runValidators: true})
-        .then(dbWorkouts => {
-            res.json(dbWorkouts)
+module.exports = function(app) {
+    // App.get to pull up info for the workouts page
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
         })
         .catch(err => {
-            console.log(err)
             res.status(400).json(err);
         });
-})
+    })
+    // App.get to pull up info for the range page
+    app.get("/api/workouts/range", ({}, res) => {
+      db.Workout.find({}).then((dbWorkout) => {
+        res.json(dbWorkout);
+      }).catch(err => {
+        res.status(400).json(err);
+      });
+    });
+    // App.post to submit new completed workouts
+    app.post("/api/workouts/", (req, res) => {
+        db.Workout.create(req.body).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+          });
+      });
+      // App.put to update workouts by MongoDB _id value and update the exercsise body
+      app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+          { _id: req.params.id }, { exercises: req.body }
+        ).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+          res.status(400).json(err);
+        });
 
-module.exports = router;
+        app.get("/exercise", function(req, res) {
+          res.sendFile(path.join(__dirname, "../public/exercise.html"));
+        });
+      
+        app.get("/stats", function(req, res) {
+          res.sendFile(path.join(__dirname, "../public/stats.html"));
+        });
+    });
+};
