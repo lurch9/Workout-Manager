5d43e30656ce6b735298c43af07a877381e92d09
lurch <alexhann17@yahoo.com>
2022-02-12 16:31:23 -0800

    updated schema

--- models/schema.js
+++ models/schema.js
@@ -1,53 +1,43 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema(
-  {
+const WorkoutSchema = new Schema({
     day: {
       type: Date,
-      default: () => new Date()
+      default: Date.now
     },
     exercises: [
-      {
-        type: {
-          type: String,
-          trim: true
-        },
-        name: {
-          type: String,
-          trim: true        },
-        duration: {
-          type: Number
-                },
-        weight: {
-          type: Number
-        },
-        reps: {
-          type: Number
-        },
-        sets: {
-          type: Number
-        },
-        distance: {
-          type: Number
-        }
+        {
+          type: {
+            type: String,
+            trim: true,
+            required: true
+          },
+          name: {
+            type: String,
+            trim: true,
+            required: true
+          },
+          duration: {
+            type: Number,
+            required: true
+          },
+          weight: {
+            type: Number
+          },
+          reps: {
+            type: Number
+          },
+          sets: {
+            type: Number
+          },
+          distance: {
+            type: Number
+          }
       }
     ]
-  },
-  {
-    toJSON: {
-      virtuals: true
-    }
-  }
-);
-
-workoutSchema.virtual("totalDuration").get(function () {
-  return this.exercises.reduce((total, exercise) => {
-    return total + exercise.duration;
-  }, 0);
-});
-
-const Workout = mongoose.model("Workout", workoutSchema);
-
-module.exports = { Workout };
+  });
+  
+  const Workout = mongoose.model("workout", WorkoutSchema);
+  
+  module.exports = Workout;
